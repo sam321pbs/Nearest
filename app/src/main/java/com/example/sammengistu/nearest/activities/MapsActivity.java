9481d3205d3884f0295abc3f5c9a09d28ef63279
Samuel Mengistu <sam321pbs@gmail.com>
2016-04-20 19:29:39 -0400

    refactor: uses GoogleApiClient to get current location

--- app/src/main/java/com/example/sammengistu/nearest/activities/MapsActivity.java
+++ app/src/main/java/com/example/sammengistu/nearest/activities/MapsActivity.java
@@ -1,6 +1,8 @@
 package com.example.sammengistu.nearest.activities;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -14,12 +16,12 @@ import com.example.sammengistu.nearest.MapListAdapter;
 import com.example.sammengistu.nearest.R;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Geocoder;
 import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
@@ -32,13 +34,15 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MapsActivity extends FragmentActivity {
+public class MapsActivity extends FragmentActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private static final String TAG = "MapActivity";
     private GoogleMap mMap;
     private ListView lv;
     private List<android.location.Address> geocodeMatches;
     private List<Address> mAddressesToShowOnMap = new ArrayList<>();
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,12 +63,20 @@ public class MapsActivity extends FragmentActivity {
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        }
+
         mMap.setMyLocationEnabled(true);
 
-        zoomOnMyLocation();
 
         ArrayList<Address> addresses =
-                AddressLab.get(getApplicationContext()).getmAddressBook();
+            AddressLab.get(getApplicationContext()).getmAddressBook();
 
         for (Address a : addresses) {
             if (a.isShowOnMap()) {
@@ -111,7 +123,7 @@ public class MapsActivity extends FragmentActivity {
         if (mMap == null) {
             // Try to obtain the map from the SupportMapFragment.
             mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.location_map))
-                    .getMap();
+                .getMap();
             // Check if we were successful in obtaining the map.
             if (mMap != null) {
                 setUpMap();
@@ -127,8 +139,8 @@ public class MapsActivity extends FragmentActivity {
                 double longitude = 0;
                 try {
                     geocodeMatches =
-                            new Geocoder(this).getFromLocationName(
-                                    addressToDisplay.getFullAddress(), 1);
+                        new Geocoder(this).getFromLocationName(
+                            addressToDisplay.getFullAddress(), 1);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -139,19 +151,18 @@ public class MapsActivity extends FragmentActivity {
                     addressToDisplay.setLongitude(longitude);
                 }
                 Marker marker = mMap.addMarker(
-                        new MarkerOptions().position(new LatLng(latitude, longitude))
-                                .visible(true)
-                                .title(addressToDisplay.getTitle())
+                    new MarkerOptions().position(new LatLng(latitude, longitude))
+                        .visible(true)
+                        .title(addressToDisplay.getTitle())
                 );
             }
         }
     }
 
     private void zoomOnMyLocation() {
-        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        double longitude = location.getLongitude();
-        double latitude = location.getLatitude();
+
+        double longitude = mLastLocation.getLongitude();
+        double latitude = mLastLocation.getLatitude();
         LatLng currentItemOnList = new LatLng(latitude, longitude);
 
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentItemOnList, 15));
@@ -161,7 +172,59 @@ public class MapsActivity extends FragmentActivity {
     protected void onResume() {
         super.onResume();
         setUpMapIfNeeded();
-      //  setUpTravelInfo();
+
+        //  setUpTravelInfo();
         ((MapListAdapter) lv.getAdapter()).notifyDataSetChanged();
+    }
+
+    @Override
+    protected void onStart() {
+        mGoogleApiClient.connect();
+
+        super.onStart();
+
+    }
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.i(TAG, "Connecting to api");
+
+        if (ActivityCompat.checkSelfPermission(
+            this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+            PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+            this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+            mGoogleApiClient);
+
+        if (mLastLocation != null) {
+            zoomOnMyLocation();
+        } else {
+            Log.i(TAG, "last location = null");
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
